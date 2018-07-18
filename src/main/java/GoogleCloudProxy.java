bd5eb9e9d849c148fe33503fff3be8222220d374
Evan Hallmark <ehallmark@gttgrp.com>
2018-07-18 14:25:49 -0700

    c

--- src/main/java/GoogleCloudProxy.java
+++ src/main/java/GoogleCloudProxy.java
@@ -1,3 +1,7 @@
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
 import java.net.URL;
 
 import static spark.Spark.*;
@@ -16,6 +20,27 @@ public class GoogleCloudProxy {
             res.type("application/force-download");
             return new URL(urlStr).openStream();
         });
+
+        get("/epo", (req, res)-> {
+            String asset = req.queryParams("asset");
+            String auth_token = req.queryParams("auth_token");
+            if(asset == null) return null;
+            URL url = new URL("http://ops.epo.org/3.2/rest-services/family/publication/docdb/"+asset);
+            System.out.println(url);
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestMethod("GET");
+            connection.setRequestProperty("Authorization","Bearer "+auth_token);
+            InputStream content = connection.getInputStream();
+            BufferedReader rd = new BufferedReader(new InputStreamReader(content));
+            String line;
+            StringBuilder result = new StringBuilder();
+            while ((line = rd.readLine()) != null) {
+                result.append(line);
+            }
+            rd.close();
+            return result.toString();
+        });
+
         awaitInitialization();
         System.out.println("Started server...");
     }
